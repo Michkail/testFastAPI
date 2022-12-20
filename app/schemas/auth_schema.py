ea7df1c83610b71f0de743aea6a36349a3e49f58
Michkail <55208634+Michkail@users.noreply.github.com>
2022-12-21 02:19:34 +0700

    fixing generate token and add fourth assets

--- app/schemas/auth_schema.py
+++ app/schemas/auth_schema.py
@@ -3,8 +3,8 @@ from pydantic import BaseModel
 
 
 class TokenSchema(BaseModel):
-    access_token = str
-    refresh_token = str
+    access_token: str
+    refresh_token: str
 
 
 class TokenPayload(BaseModel):
